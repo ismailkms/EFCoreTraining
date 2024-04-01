abb4398adadf4e08684df1119f1dc7a0b70125d8
İsmail Abdullah Kamış <81959794+ismailkms@users.noreply.github.com>
2024-04-01 22:46:33 +0300

    Add project files.

---
+++ Code_First/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+//Proje ayağa kalktığında otomatik Migration oluşturur
+ECommerceDbContext context = new();
+await context.Database.MigrateAsync();
+
+
+public class ECommerceDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("server=localhost\\SQLEXPRESS;database=ECommerceDb;integrated security=true;TrustServerCertificate=True");
+        //Google Connection String yazarsan hangi db ile çalıyorsan onun Connection Stringini öğrenebilirsin
+    }
+}
+
+//Entity
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public float Price { get; set; }
+}
+//Entity
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
